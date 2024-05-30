0aecfeceed46c7c762396f4b963694ddfbe99f33
beomsooo.kim <lightjd@naver.com>
2024-05-30 15:05:56 +0900

    [Refactor] Page42

--- 20240530_TDD_Baseball/baseball.cpp
+++ 20240530_TDD_Baseball/baseball.cpp
@@ -6,21 +6,28 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
+		AssertIlligalArgument(guessNumber);
+	}
+	void AssertIlligalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3)
 			throw length_error("Must be three letters.");
 
 		for (auto ch : guessNumber)
 		{
-			if (!std::isdigit(ch))
+			if (std::isdigit(ch) == false)
 				throw std::invalid_argument("Must be number.");
 		}
 
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2])
+		if (IsDuplicatedNumber(guessNumber))
 		{
 			throw std::invalid_argument("Duplicated number in guessNumber.");
-
 		}
+	}
+	bool IsDuplicatedNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
 	}
 };
